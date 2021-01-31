a41d79593d8f7d6560c3fd7c7416e1a524bfee1b
dolphius21 <johnrudolphlee@gmail.com>
2021-01-31 19:32:57 +0800

    done with Node Module System Section

--- 01_node_module_system/app.js
+++ 01_node_module_system/app.js
@@ -11,13 +11,64 @@
 
 
 // Loading a Module
-const logger = require('./logger'); // require() => returns the object that was exported from the target module.
+// const Logger = require('./logger'); require() => returns the object that was exported from the target module.
 // console.log(logger);
 
 // you chose to export a single method instead of a object
-logger('Hello Rudolph!') // instead of logger.log('Hello Rudolph!'); 
+// logger('Hello Rudolph!') instead of logger.log('Hello Rudolph!'); 
 
 // Path Module => a Node builtin module.
 const path = require('path'); // The path module provides utilities for working with the file and directory paths.
 const pathObj = path.parse(__filename)
 console.log(pathObj);
+
+// OS Module => provides operating system-related utility methods and properties. 
+const os = require('os');
+const totalMemory = os.totalmem(); // Returns the total amount of system memory in bytes as an integer.
+const freeMemory = os.freemem(); // Returns the amount of free system memory in bytes as an integer.
+
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
+
+// File System Module
+const fs = require('fs');
+// const files = fs.readdirSync('./'); // fs.readdirSync() => (Synchronous) Returns all the files and folders in the current folder.
+const files = fs.readdir('./', (err, files) => { // fs.readdir() => (Asynchronous) Returns all the files and folders in the current folder.
+   if (err) {
+      console.log('Error', err);
+   } else {
+      console.log('Result', files);
+   } 
+});
+
+// Events Module
+// const EventEmitter = require('events'); // EventEmitter Class
+// const emitter = new EventEmitter(); Instantiate a new emitter object.
+const Logger = require('./logger');
+const logger = new Logger();
+
+// Register a listener
+logger.on('myMessage', (e) => {
+   console.log('an event occurred!', e);
+});
+
+logger.log('This is a message');
+
+// HTTP Module
+const http = require('http');
+
+const server = http.createServer( (req, res) => {
+   if (req.url === '/') {
+      res.write('Wazzzupp!!');
+      res.end()
+   }
+
+   if (req.url === '/api/courses') {
+      res.write(JSON.stringify([1, 2, 3]));
+      res.end()
+   }
+});
+
+server.listen(3000);
+
+console.log('Listening on port 3000...');
--- 01_node_module_system/logger.js
+++ 01_node_module_system/logger.js
@@ -1,17 +1,28 @@
+// EventEmitter Class
+const EventEmitter = require('events');
+
 // Creating a Module
 const url = 'http://mylogger.io/log';
 
-function log(message) {
-   // Send an HTTP request
-   console.log(message);
+class Logger extends EventEmitter {
+   log(message) {
+      // Send an HTTP request
+      console.log(message);
+   
+      // Raise an event
+      this.emit('myMessage', { id: 1, data: "This is a data!" })
+   }
 }
+
+module.exports = Logger;
+
 
 // export variables/functions using module.exports
 // module.exports.log = log; // calls a log method and setting it to the log function.
-module.exports.endPoint = url; // (endPoint) you can change the name of the method depending on what you want to call it when using it on other modules/files.
+// module.exports.endPoint = url; (endPoint) you can change the name of the method depending on what you want to call it when using it on other modules/files.
 
 // instead of exporting an object to another module, you may want to export a single function if your only exporting a single method.
-module.exports = log; // instead of module.exports.log = log;
+// module.exports = log; instead of module.exports.log = log;
 
 /* node does not execute our code directly, node wraps the code inside each module to a function, also known as a (Module Wrapper Function):
    ex.
@@ -25,5 +36,5 @@ module.exports = log; // instead of module.exports.log = log;
    }
 */
 
-console.log(__filename);
-console.log(__dirname);
+// console.log(__filename);
+// console.log(__dirname);
